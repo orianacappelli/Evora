7616c3f008f6ad3714c1b8da7f9290bc7b0e8e6c
orianacappelli <orianacappelli1@gmail.com>
2024-10-03 19:23:02 -0300

    si

--- Downloads/ecommerce/ecommerce/src/app/about/page.js
+++ Downloads/ecommerce/ecommerce/src/app/about/page.js
@@ -18,7 +18,7 @@ const About = async () => {
       />
       </div>
       <div className="container-about h-screen">
-        <h1 className="titulo">Bienvenidos a Evora</h1>
+        <h1 className="titulo">Bienvenidos a Evora!!</h1>
         <p className="texto">En Evora, creemos que el lujo no es solo llevar joyas hermosas, sino abrazar una elegancia atemporal que trasciende las tendencias. Fundada con la visión de ofrecer piezas exclusivas y de alta gama, estamos dedicados a crear joyas exquisitas que capturen la esencia de la sofisticación y el refinamiento.</p>
         <h2 className="seg-titulo">Nuestro Compromiso</h2>
         <p className="texto">Estamos comprometidos con la sostenibilidad y las prácticas éticas. Desde los materiales que seleccionamos hasta la forma en que creamos nuestras joyas, buscamos tener un impacto positivo tanto en el medio ambiente como en las comunidades con las que trabajamos. Cada pieza de Evora refleja nuestra dedicación al lujo responsable.</p>
