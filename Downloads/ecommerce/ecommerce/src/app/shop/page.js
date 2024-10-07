9835268f0d192029bf4da6cf545ffe5083d107b5
orianacappelli <orianacappelli1@gmail.com>
2024-10-07 03:10:41 -0300

    mejore css

--- Downloads/ecommerce/ecommerce/src/app/shop/page.js
+++ Downloads/ecommerce/ecommerce/src/app/shop/page.js
@@ -1,10 +1,27 @@
-import React from 'react'
 
-export default function page() {
+import ProductCard from '@/app/components/ProductCard/ProductCard';
+import style from "./Shop.modules.css";
+import { getAllProductsDB } from '@/app/actions';
+
+export default async function page() {
+  const response = await getAllProductsDB()
   return (
-    <div>
+    <section>
         
-    </div>
+        <div className='container-sp'>
+          <div className='articulos'>
+            <h2>Anillos</h2>
+            <h2>Pulseras</h2>
+            <h2>Collares</h2>
+          </div>
+          <div className='productos-td'>
+            {response.products && response.products.map((item, index) => (
+              <ProductCard key={index} item={item} />
+            ))}
+          </div>
+        </div>
+      
+    </section>
   )
 }
 
